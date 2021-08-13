3fa13a96f03ea352839cc23514ed9611e15c62a8
Veekshith-Thoram <veekshiththoram@gmail.com>
2021-08-13 17:30:53 +0530

    initial commit,the initial css, the main logic and the app.js completed

---
+++ app.js
@@ -0,0 +1,48 @@
+const billAmt = document.querySelector("#bill-amount");
+const cashGiven = document.querySelector("#cash-given");
+const checkBtn = document.querySelector("#check");
+const errorMessage = document.querySelector("#error-message");
+const availableNotes = [2000,500,100,50,20,10,5,1];
+const noOfNotes = document.querySelectorAll(".no-of-notes");
+
+function clickHandler(){
+    validateBillAmount();
+}
+
+function validateBillAmount(){
+    hideMessage();
+    if (billAmt.value>0){
+        validateBillVsCashPaid()
+    } else {
+        showMessage("Invalid bill amount.")
+    }
+}
+function validateBillVsCashPaid(){
+    if(cashGiven.value >= billAmt.value){
+        const amountToBeReturned = cashGiven.value - billAmt.value;
+        calculateChange(amountToBeReturned);
+    } else{
+        showMessage("The cash given is not sufficient!!")
+    }
+}
+
+function hideMessage(){
+    errorMessage.style.display = "none";
+}
+
+function calculateChange(amountToBeReturned){
+    for(let i=0;i<availableNotes.length;i++){
+        const numberOfNotes = Math.trunc(amountToBeReturned/availableNotes[i]);
+        amountToBeReturned %= availableNotes[i];
+        if(numberOfNotes>0){
+        noOfNotes[i].innerText = numberOfNotes;
+        }
+    }
+}
+
+function showMessage(message){
+    errorMessage.style.display = "block";
+    errorMessage.innerText = message;
+}
+
+checkBtn.addEventListener("click", clickHandler)
